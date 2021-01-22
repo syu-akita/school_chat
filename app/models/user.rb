86699d0977c811b32563d664e54637f5d45dd74c
syu-akita <syu.akita.0612@gmail.com>
2021-01-22 12:25:02 +0900

    新規登録の際のエラー文の表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
   validates :name,          presence: true
   validates :group_id,      presence: true
   validates :status_id,     presence: true
-
+  validates :group_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :group
