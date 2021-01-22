36472e6c4c959ff4de7fdcafc3beb8635ed77a9e
syu-akita <syu.akita.0612@gmail.com>
2021-01-22 11:22:53 +0900

    active_hash　の導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name,          presence: true
+  validates :group_id,      presence: true
+  validates :status_id,     presence: true
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :group
+  belongs_to :status
 end
