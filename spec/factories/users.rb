3a2beae5a0b138ce383608adf95050262d696c77
syu-akita <syu.akita.0612@gmail.com>
2021-01-25 16:10:09 +0900

    FactoryBotの作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    name {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    status_id { 3 }
+    group_id { 2 }
+  end
+end
