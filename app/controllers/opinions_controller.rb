c448e7563f030fa9e156099b80ab56b87f861ee6
syu-akita <syu.akita.0612@gmail.com>
2021-01-29 17:08:22 +0900

    目安箱に意見を投票できるようにする

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -5,4 +5,15 @@ class OpinionsController < ApplicationController
   def new
     @opinion = Opinion.new
   end
+
+  def create
+    @opinion = Opinion.new(opinion_params)
+    @opinion.save
+  end
+
+  private
+
+  def opinion_params
+    params.require(:opinion).permit(:subject, :text)
+  end
 end
