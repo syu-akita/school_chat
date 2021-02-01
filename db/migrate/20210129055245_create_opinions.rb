e680af1dbe220bb68a521f06b57c0bf72abfa035
syu-akita <syu.akita.0612@gmail.com>
2021-02-01 20:01:03 +0900

    Revert "Merge pull request #8 from syu-akita/意見ボックスの作成"
    This reverts commit b3ab24e06299b1e461841363c36a6d813e6afbff, reversing
    changes made to 0f3633de17a3e7fed7fcdad5870d0bdcf149d0b3.

--- db/migrate/20210129055245_create_opinions.rb
+++
@@ -1,9 +0,0 @@
-class CreateOpinions < ActiveRecord::Migration[6.0]
-  def change
-    create_table :opinions do |t|
-      t.string :subject,        null: false
-      t.text   :text,           null: false
-      t.timestamps
-    end
-  end
-end
