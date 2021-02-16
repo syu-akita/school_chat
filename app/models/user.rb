00adbf57af796c4d85ae8d20168f2bc0ab302f8b
syu-akita <syu.akita.0612@gmail.com>
2021-02-16 21:24:00 +0900

    userモデル　バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true
-  validates :group_id,      presence: true
-  validates :status_id,     presence: true
+         
   validates :group_id, numericality: { other_than: 1 }
   validates :status_id, numericality: { other_than: 1 }
+
+  with_options presence: true do
+    validates :name
+    validates :group_id
+    validates :status_id
+  end
 
   has_many :room_users
   has_many :rooms, through: :room_users
